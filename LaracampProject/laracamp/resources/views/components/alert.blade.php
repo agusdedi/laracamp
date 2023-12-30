dbf9fe987ba85672a41a260ec2476f2c1850f1e6
Agus Dedi <154070380+agusdedi@users.noreply.github.com>
2023-12-31 06:16:49 +0800

    Delete LaracampProject directory

--- LaracampProject/laracamp/resources/views/components/alert.blade.php
+++
@@ -1,6 +0,0 @@
-@if ($message =  Session::get('error'))
-    <div class="alert alert-danger alert-dismissible fade show" role="alert">
-        <strong>{{ $message }}</strong>
-        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-    </div>
-@endif

2cc66db9592fd71282ebb52315e4e55439ae43fe
agusdedi <dedipriyana16@gmail.com>
2024-01-10 23:26:43 +0800

    feat: integrate discount with checkout page

--- laracamp/app/Http/Requests/User/Checkout/Store.php
+++ laracamp/app/Http/Requests/User/Checkout/Store.php
@@ -22,14 +22,13 @@ class Store extends FormRequest
      */
     public function rules(): array
     {
-        $expiredValidation = date('Y-m', time());
-
         return [
             'name' => 'required|string',
             'email' => 'required|email|unique:users,email,'.Auth::id().',id',
             'occupation' => 'required|string',
             'phone' => 'required|string',
             'address' => 'required|string',
+            'discount' => 'nullable|string|exists:discounts,code,deleted_at,NULL'
         ];
     }
 }
